dd3a6cbb0105e99d0f4e3745b3d1540f819ee2fb
hj24 <huangjian24@foxmail.com>
2019-05-08 15:11:43 +0800

    finish the frame

--- line.py
+++ line.py
@@ -14,24 +14,48 @@ class LineCounter(object):
 		self.line_count = 0
 		self.file_count = 0
 
+	def filter_mod(self, filter_list):
+		print(filter_list)
+
+	def specific_mod(self, suffix_list):
+		print(suffix_list)
+
+	def normal_mod(self):
+		pass
+
+	def show_results(self):
+		print(f'file count: {self.file_count}')
+		print(f'line count: {self.line_count}')
 
 def main():
 	__usage__ = "count the amount of lines and files under the current directory"
 	parser = argparse.ArgumentParser(description=__usage__)
-	group = parser.add_mutually_exclusive_group()
-	parser.add_argument("x", help="base", type=int)
-	parser.add_argument("y", help="exp", type=int)
-	group.add_argument("-v", "--verbosity", action="store_true")
-	group.add_argument("-q", "--quiet", action="store_true")
-	args = parser.parse_args()
-	ans = args.x * args.y
-	if args.verbosity:
-		print(f"{args.x} to the power of {args.y} equals {ans}")
-	elif args.quiet:
-		print(f"{args.x} * {args.y} == {ans}")
-	else:
-		print(ans)
 
+	group = parser.add_mutually_exclusive_group()
+	group.add_argument("-s", "--suffix", type=str, 
+						help="count by suffix file name, format: .suffix1.suffix2... e.g: .cpp.py (without space)")
+	group.add_argument("-f", "--filter", type=str, 
+						help="count without filter name, format: .suffix1.suffix2... e.g: .cpp.py (without space)")
+	
+	args = parser.parse_args()
+
+	current_dir = os.getcwd()
+	counter = LineCounter(current_dir)
+
+	if args.suffix == None:
+		args_list = args.filter.split('.')
+		args_list.remove('')
+		counter.filter_mod(args_list)
+	elif args.filter == None:
+		args_list = args.suffix.split('.')
+		args_list.remove('')
+		counter.specific_mod(args_list)
+	else:
+		print(f'Search in {current_dir}' + f'{os.sep}')
+		counter.normal_mod()
+
+	counter.show_results()
+		
 if __name__ == '__main__':
 	main()
 
